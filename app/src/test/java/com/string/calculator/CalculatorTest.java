4ee1f12cabc3d3def1b17fb95441209ed4960754
oh980225 <oh980225@gmail.com>
2022-04-16 17:19:11 +0900

    test: CalculatorTest -> executeForTheRestInStack()

--- app/src/test/java/com/string/calculator/CalculatorTest.java
+++ app/src/test/java/com/string/calculator/CalculatorTest.java
@@ -1,5 +1,6 @@
 package com.string.calculator;
 
+import com.string.calculator.collection.FormulaStack;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -13,5 +14,20 @@ class CalculatorTest {
     var actual = calculator.executeForBinary("12", "5", OperatorSign.plus);
 
     assertThat(actual, is("17"));
+  }
+
+  @Test
+  void executeForTheRestInStack() {
+    var calculator = new Calculator();
+    var formulaStack = new FormulaStack();
+    formulaStack.put("1");
+    formulaStack.put("+");
+    formulaStack.put("5");
+    formulaStack.put("-");
+    formulaStack.put("1");
+
+    var actual = calculator.executeForTheRestInStack(formulaStack);
+
+    assertThat(actual, is("5"));
   }
 }
