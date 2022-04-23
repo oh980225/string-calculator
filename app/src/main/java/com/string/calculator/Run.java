193bf68ceecb51298fb49a7bbc08e734aef3bd04
oh980225 <oh980225@gmail.com>
2022-04-23 23:39:56 +0900

    refactor: Organize flow

--- app/src/main/java/com/string/calculator/Run.java
+++ app/src/main/java/com/string/calculator/Run.java
@@ -9,10 +9,22 @@ import java.util.Stack;
  * 얘는 딱 인풋을 받으면 숫자들은 숫자 스택에, 연산자는 연산자스택에 넣어주는 역할만 하고싶은데 <-- 이것도 책임이 많은편인건가 내가 설계한 계산기 특성상 높은 우선순위의
  * 연산자가 있으면 연산을 해줘야 하는 상황....
  */
+
+// [FLOW]
+// 1. 입력을 char로 다 나눠준다.
+// 2. 나눈 char을 공백을 기준으로 숫자 스택과 연산자 스택에 나눠서 넣어준다.
+// 3. 이때 우선순위 연산자가 있고, 숫자 스택의 사이즈가 연산자 스택의 사이즈보다 크다면 미리 숫자 2개와 연산자 1개로
+//    연산을 진행하고 그 결과를 숫자 스택에 넣어준다.
+// 4. 숫자 스택과 연산자 스택을 모두 뒤집으며 연산하고 최종 결과를 반환한다.
+
+// 상태가 변하는 걸 위로 올리니 책임을 어떻게 나눠야할지 모르겠다...
+// 상태와 관련된 책임을 나누는 순간 depth가 추가된다...
+
 public class Run {
   private Stack<String> numberStack = new Stack<>();
   private Stack<OperatorSign> operatorSignStack = new Stack<>();
   private final StringBuilder numberPiece = new StringBuilder();
+
   private final Calculate calculate;
 
   public Run() {
@@ -61,6 +73,7 @@ public class Run {
       String pop = numberStack.pop();
       temp.add(pop);
     }
+
     numberStack = temp;
   }
 
@@ -111,7 +124,6 @@ public class Run {
       return false;
     }
 
-    // 빠져야 하는 코드인데 현재 코드를 고치면 안됨...
     if (operatorSignStack.size() >= numberStack.size()) {
       return false;
     }
