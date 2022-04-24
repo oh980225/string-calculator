7a1feadeaf9c90c2594ec454091057186380103f
oh980225 <oh980225@gmail.com>
2022-04-24 13:54:06 +0900

    refactor: Extract ExistHighOperator

--- app/src/main/java/com/string/calculator/ExistHighOperator.java
+++ app/src/main/java/com/string/calculator/ExistHighOperator.java
@@ -1,2 +1,15 @@
-package com.string.calculator;public class ExistHighOperator {
+package com.string.calculator;
+
+import java.util.Stack;
+
+class ExistHighOperator {
+  boolean check(Stack<OperatorSign> operatorSignStack) {
+    if (operatorSignStack.isEmpty()) {
+      return false;
+    }
+
+    OperatorSign lastOperator = operatorSignStack.peek();
+
+    return lastOperator == OperatorSign.divide || lastOperator == OperatorSign.multiply;
+  }
 }
--- app/src/test/java/com/string/calculator/ExistHighOperatorTest.java
+++ app/src/test/java/com/string/calculator/ExistHighOperatorTest.java
@@ -1,4 +1,22 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.string.calculator;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Stack;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 class ExistHighOperatorTest {
-  
+  @Test
+  void check() {
+    var existHighOperator = new ExistHighOperator();
+    Stack<OperatorSign> stack = new Stack<>();
+    stack.push(OperatorSign.plus);
+    stack.push(OperatorSign.subtract);
+    stack.push(OperatorSign.multiply);
+
+    var actual = existHighOperator.check(stack);
+    assertThat(actual, is(true));
+  }
 }
